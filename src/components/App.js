c075ed2706520997b7a2b5c34f7765ca940aae07
simeo russo <simeo94@hotmail.com>
2020-10-12 14:09:40 +0100

    created list component for images

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 
 class App extends React.Component{
 
-  onSerachSubmit(term){
-    axios.get(`https://api.unsplash.com/search/photos`, {
-      params: { query: term} ,
-      headers:{
-        Authorization: `Client-ID ${process.env.REACT_APP_API_KEY}`
-      }
-    });
+  state = {
+    images: []
   }
+
+  onSerachSubmit = async (term) => {
+    const res = await unsplash.get(`/search/photos`, {
+      params: { query: term},
+    });
+
+    this.setState({images: res.data.results});
+  };
 
   render(){
     return(
       <div className="ui container" style={{marginTop:'15px'}}>
         <SearchBar onSubmit={this.onSerachSubmit} />
+        <ImageList images={this.state.images}/>
       </div>
     )
   }
